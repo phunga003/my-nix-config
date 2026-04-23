2f5704505ce3aad2f5964a749f043bcc3c30f1a2
Khai <phunga.prod@gmail.com>
2026-04-23 20:45:54 +0000

    add language supports

--- home/default.nix
+++ home/default.nix
@@ -1,24 +1,46 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 let 
   username = "nixos";
 in
 { 
   home.username = username;
   home.homeDirectory = "/home/${username}";
-  home.stateVersion = "24.11";  
+  home.stateVersion = "24.05";  
 
   home.packages = with pkgs; [
     git
     clang-tools   
     jetbrains-mono
+    home-manager
+    erdtree
+    nixd
   ];
 
+  targets.genericLinux.enable = true;
+
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+    matchBlocks."*" = {
+      addKeysToAgent = "yes";
+    };
+  };
+  
   programs.bash = {
     enable = true;
     sessionVariables = {
       COLORTERM = "truecolor";
     };
-  }
+
+    # if Nix is running on WSL or non-standard
+    initExtra = lib.mkIf config.targets.genericLinux.enable ''
+      source ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+
+      if [ -z "$SSH_AUTH_SOCK" ]; then
+        eval $(ssh-agent -s) > /dev/null
+      fi
+    '';
+  };
 
   programs.starship = {
     enable = true;
@@ -33,21 +55,34 @@ in
   programs.helix = {
     enable = true;
     settings = {
-      theme = "catppuccin_mocha";
-      editor.lsp.display-messages = true;
+      theme = "catppuccin_macchiato";
+      editor = {
+        lsp.display-messages = true;
+        color-modes = true;
+        inline-diagnostics = {
+          cursor-line = "warning";
+          other-lines = "error";
+        };
+      };
     };
     languages = {
-      language = [{
-        name = "c";
-        language-servers = [ "clangd" ];
-      }];
+      language = [
+        {
+          name = "c";
+          language-servers = [ "clangd" ];
+        }
+        {
+          name = "nix";
+          language-servers = [ "nixd" ];
+        }
+      ];
     };
   };
 
   programs.zellij = {
     enable = true;
     settings = {
-      theme = "catppuccin-mocha";
+      theme = "catppuccin-macchiato";
     };
   };
 }
